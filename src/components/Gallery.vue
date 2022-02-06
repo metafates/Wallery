fe64168cfcf1150bf4d78c93b08320d79c13add0
Lain <vvz.dev@gmail.com>
2022-02-06 21:37:09 +0300

    masonry layout added!! finally

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,26 +1,37 @@
 <template>
     <v-container fill-height class="justify-center">
-        <Loader v-if="loading" />
-        <v-row v-else>
+        <v-overlay v-if="loading" color="white" opacity="1">
+            <Loader />
+        </v-overlay>
+        <v-row v-masonry="'gallery'" transition-duration="0.3s" item-selector=".wallpaper">
             <v-col
-                v-for="w in wallpapers"
-                :key="w.max"
+                v-masonry-tile
+                v-for="(w, i) in wallpapers"
+                :key="i"
                 sm="4"
                 cols="6"
+                class="wallpaper"
             >
                 <v-hover v-slot="{hover}">
-                    <a :href="w.max" target="_blank" class="text-decoration-none" >
+                    <v-card
+                        height="100%"
+                        hover
+                        :href="w.max"
+                        target="_blank"
+                    >
                         <v-img
-                            :class="{'scale elevation-0': hover}"
-                            class="elevation-3 smooth"
                             :src="w.max"
                             :lazy-src="w.min"
+                            @load="$redrawVueMasonry('gallery')"
                         >
-                            <div :class="hover ? 'd-flex' : 'd-none'" class="justify-center align-end" style="height: 100%">
-                                <span class="white pa-3">
-                                    {{w.dimensions}}
-                                </span>
-                            </div>
+                            <v-expand-transition>
+                                <div
+                                    v-if="hover"
+                                    class="d-flex justify-center elevation-3 white align-center v-card--reveal text-subtitle-1"
+                                >
+                                    <span>{{w.dimensions}}</span>
+                                </div>
+                            </v-expand-transition>
                             <template v-slot:placeholder>
                                 <v-row
                                     class="fill-height ma-0"
@@ -34,9 +45,8 @@
                                 </v-row>
                             </template>
                         </v-img>
-                    </a>
+                    </v-card>
                 </v-hover>
-
             </v-col>
         </v-row>
     </v-container>
@@ -52,22 +62,15 @@ export default {
     data() {
         return {
             wallpapers: 30,
-            loading: true
+            loading: true,
+            containerId: 'gallery'
         }
     },
     async mounted() {
         this.wallpapers = await getWallpapers()
-        this.loading = false
+        setTimeout(() => {
+            this.loading = false
+        }, 2000)
     }
 }
 </script>
-
-<style>
-.smooth {
-    transition: .2s;
-}
-
-.scale {
-    transform: scale(1.015);
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import { VueMasonryPlugin } from "vue-masonry";
 
 Vue.config.productionTip = false
+
+Vue.use(VueMasonryPlugin);
 
 new Vue({
     vuetify,
