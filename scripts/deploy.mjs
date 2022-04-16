ad059f7f69affd2a67cb45be46559d03249ab0dd
Lain <vvz.dev@gmail.com>
2022-04-16 20:46:04 +0300

    fix: deploy script updated

--- scripts/deploy.mjs
+++ scripts/deploy.mjs
@@ -6,7 +6,7 @@ import * as fs from "fs";
         await execa("git", ["checkout", "--orphan", "gh-pages"]);
         // eslint-disable-next-line no-console
         console.log("Building started...");
-        await execa("pnpm", ["run", "build"]);
+        await execa("npm", ["run", "build"]);
         // Understand if it's dist or build folder
         const folderName = fs.existsSync("dist") ? "dist" : "build";
         await execa("git", ["--work-tree", folderName, "add", "--all"]);
